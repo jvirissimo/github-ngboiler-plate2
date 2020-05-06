38e88c57985392d1497c9b5effba85cf104da92c
jvirissimo <jvirissimo@gmail.com>
2020-05-06 15:22:53 -0500

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/dashboard',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-  { path: 'users', loadChildren: './users/users.module#UsersModule' },
-  { path: 'account-settings', loadChildren: './account-settings/account-settings.module#AccountSettingsModule' },
   {
     path: 'login',
     loadChildren: './login/login.module#LoginModule',
     data: { showHeader: false, showSidebar: false }
+  },
+  { path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardModule'
+  },
+  { path: 'users',
+    loadChildren: './users/users.module#UsersModule'
+  },
+  { path: 'account-settings',
+    loadChildren: './account-settings/account-settings.module#AccountSettingsModule'
   },
   {
     path: 'registration',
