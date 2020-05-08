9c1fd7de4670f8ed1e20a43003758568a5e20bf0
jvirissimo <jvirissimo@gmail.com>
2020-05-08 10:18:19 -0500

    1 file modified

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-//import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    //FlexLayoutModule,
     RouterModule
   ],
   exports: [
