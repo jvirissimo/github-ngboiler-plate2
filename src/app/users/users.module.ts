9a66ef16c3cf2aeeb8681ede1abd3f38efd2261c
jvirissimo <jvirissimo@gmail.com>
2020-05-11 14:27:21 -0500

    3 files modified

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 
-import { UsersRoutingModule } from './users-routing.module';
-import { UsersComponent } from './users.component';
+// import { UsersRoutingModule } from './users-routing.module';
+// import { UsersComponent } from './users.component';
 
-@NgModule({
-  imports: [
-    CommonModule,
-    UsersRoutingModule
-  ],
-  declarations: [UsersComponent]
-})
-export class UsersModule { }
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     UsersRoutingModule
+//   ],
+//   declarations: [UsersComponent]
+// })
+// export class UsersModule { }
