dad99b52ad23c2d7d5c025c0a56652ce500a1660
jvirissimo <jvirissimo@gmail.com>
2020-05-29 16:17:07 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,9 +21,35 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     RouterModule.forRoot([]),
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// import {
+//   MatButtonModule,
+//   MatMenuModule,
+//   MatToolbarModule,
+//   MatIconModule,
+//   MatCardModule
+// } from '@angular/material';
+
+// @NgModule({
+//   imports: [
+//     MatButtonModule,
+//     MatMenuModule,
+//     MatToolbarModule,
+//     MatIconModule,
+//     MatCardModule
+//   ],
+//   exports: [
+//     MatButtonModule,
+//     MatMenuModule,
+//     MatToolbarModule,
+//     MatIconModule,
+//     MatCardModule
+//   ]
+// })
+// export class MaterialModule {}
